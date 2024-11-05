510428519da0c3038684acfa70bfae7a31bcba4f
Bastien Avrillon <bastien.avrillon@etu.minesparis.psl.eu>
2024-11-05 14:28:12 +0100

    Premier commit de Messenger

---
+++ messenger.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+server = {
+    'users': [
+        {'id': 1, 'name': 'Alice'},
+        {'id': 2, 'name': 'Bob'}
+    ],
+    'channels': [
+        {'id': 1, 'name': 'Town square', 'member_ids': [1, 2]}
+    ],
+    'messages': [
+        {
+            'id': 1,
+            'reception_date': datetime.now(),
+            'sender_id': 1,
+            'channel': 1,
+            'content': 'Hi 👋'
+        }
+    ]
+}
+
+print('=== Messenger ===')
+print('x. Leave')
+choice = input('Select an option: ')
+if choice == 'x':
+    print('Bye!')
+else:
+    print('Unknown option:', choice)
