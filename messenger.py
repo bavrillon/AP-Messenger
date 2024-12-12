c9fdbd906748c170e952113aa4f295eb9bd3d34b
Bastien Avrillon <bastien.avrillon@etu.minesparis.psl.eu>
2024-12-12 11:11:51 +0100

    Màj du 12/12

--- messenger.py
+++ messenger.py
@@ -74,6 +74,43 @@ class Server :
         server_dico = {"users": [user_User.to_dico() for user_User in self.users],"channels": [channel_Channel.to_dico() for channel_Channel in self.channels],"messages": [message_Message.to_dico() for message_Message in self.messages]}
         with open(file, "w") as f:
             json.dump(server_dico, f)
+    
+    def create_user(self,names):
+        new_users_names_draft = names.split(',')
+        new_users_names = [name.strip() for name in new_users_names_draft]
+        for name_user in new_users_names :
+            n = max([user.id for user in server.users])+1
+            self.users.append(User(n,name_user))
+        self.save(JASON_FILE_NAME)
+
+    def ban_user(self,ID_banned_users):
+        ID_banned_users_draft = ID_banned_users.split(',')
+        ID_banned_users = [int(ID.strip()) for ID in ID_banned_users_draft]
+        index_banned_users = []
+        for index,user in enumerate(self.users) :
+            if user.id in ID_banned_users :
+                index_banned_users.append(index)
+        index_banned_users.sort(reverse=True)
+        for index in index_banned_users:
+            self.users.pop(index)
+        self.save(JASON_FILE_NAME)
+    
+    def create_channel(self):
+        channel = input('Name of the new channel: ')
+        first_member_id = int(input('ID of the first user belonging to the new channel: '))
+        member_ids = [first_member_id]
+        while True:
+            choice = input('Add a member (yes/no) ? : ')
+            if choice == 'no':
+                break
+            elif choice != 'yes':
+                print('Unknown option:', choice)
+                continue
+            member_ids.append(int(input('ID of the next user belonging to the new channel: ')))
+        n = max([channel.id for channel in server.channels])+1
+        server.channels.append(Channel(n+1,channel,member_ids))
+        print('The new channel have successfully been created !')
+        self.save(JASON_FILE_NAME)
 
     @classmethod
     def load(cls, file):
@@ -87,102 +124,81 @@ class Server :
         server_Server = cls([User.from_dico(user_dico) for user_dico in server_dico['users']],[Channel.from_dico(channel_dico) for channel_dico in server_dico['channels']],[Message.from_dico(message_dico) for message_dico in server_dico['messages']])
         return(server_Server)
 
-JASON_FILE_NAME = 'server.json'
-
-server = Server.load(JASON_FILE_NAME)
-
-def creation_liste_user():
-    first_member_id = int(input('ID of the first user belonging to the new channel: '))
-    member_ids = [first_member_id]
-    while True:
-        choice = input('Add a member (yes/no) ? : ')
-        if choice == 'no':
-            break
-        elif choice != 'yes':
+class Client :          #Messenger app
+    def __init__(self,server:'Server'):
+        self.server = server
+        
+    def __repr__(self):
+        return(f'Client(server={self.server})')
+    
+    def display_users(self):
+        print('\nUser list\n-------')
+        for user in self.server.users :
+            print(user.id,' - ',user.name)
+        print('\nn. Create user\nb. Ban user\nx. Main menu')
+        choice = input('Select an option: ')
+        if choice == 'n':
+            new_names = input('Name of the new users (separators = ,) : ')
+            self.server.create_user(new_names)
+            self.main_menu()
+        elif choice == 'x':
+            self.main_menu()
+        elif choice == 'b':
+            banned_id = input('ID of the banned users (separators = ,) : ')
+            self.server.ban_user(banned_id)
+            self.main_menu()
+        else:
             print('Unknown option:', choice)
-            continue
-        member_ids.append(int(input('ID of the next user belonging to the new channel: ')))
-    return member_ids
 
-def create_user(names):
-    new_users_names_draft = names.split(',')
-    new_users_names = [name.strip() for name in new_users_names_draft]
-    for name_user in new_users_names :
-        n = max([user.id for user in server.users])+1
-        server.users.append(User(n,name_user))
-    server.save(JASON_FILE_NAME)
+    def display_messages(self):
+        channel = int(input('Name of the channel: '))
+        print('\nMessages of the channel\n-------')
+        for message in self.server.messages :
+            if message.channel == channel :
+        #       print(message.id',' -\nReception date : ',message.reception_date,' -\nsender id : ',message.sender_id,'\n',message.content)
+                print(f"{message.id} -\nReception date : {message.reception_date}\nsender id : {message.sender_id}\n{message.content}")
+        print('x. Main menu')
+        choice = input('Select an option: ')
+        if choice == 'x':
+            self.main_menu()
+        else:
+            print('Unknown option:', choice)
 
-def create_channel():
-    channel = input('Name of the new channel: ')
-    members = creation_liste_user()
-    n = max([channel.id for channel in server.channels])+1
-    server.channels.append(Channel(n+1,channel,members))
-    print('The new channel have successfully been created !')
-    server.save(JASON_FILE_NAME)
-    main_menu()
+    def display_channels(self):
+        print('\nChannels list\n-------')
+        for channel in self.server.channels :
+            print(f"{channel.id} - {channel.name} : {channel.members_ids}")
+        print('\nn. Create channel\nx. Main menu')
+        choice = input('Select an option: ')
+        if choice == 'n':
+            self.server.create_channel()
+            self.main_menu()
+        elif choice == 'x':
+            self.main_menu()
+        else:
+            print('Unknown option:', choice)
+    
+    def main_menu(self):
+        print('=== Messenger ===')
+        print('\n1. See users\n2. See channels\n3. See messages\nx. Leave')
+        choice = input('Select an option: ')
+        if choice == 'x':
+            print('Bye!')
+        elif choice == '1':
+            self.display_users()
+        elif choice == '2':
+            self.display_channels()
+        elif choice == '3':
+            self.display_messages()
+        else:
+            print('Unknown option:', choice)
+    
 
-def display_users():
-    print('\nUser list\n-------')
-    for user in server.users :
-        print(user.id,' - ',user.name)
-    print('\nn. Create user\nx. Main menu')
-    choice = input('Select an option: ')
-    if choice == 'n':
-        names = input('Name of the new users (separators = ,) : ')
-        create_user(names)
-        main_menu()
-    elif choice == 'x':
-        main_menu()
-    else:
-        print('Unknown option:', choice)
+JASON_FILE_NAME = 'server.json'
+server = Server.load(JASON_FILE_NAME)
+client = Client(server)
 
-def display_messages():
-    channel = int(input('Name of the channel: '))
-    print('\nMessages of the channel\n-------')
-    for message in server.messages :
-        if message.channel == channel :
-    #       print(message.id',' -\nReception date : ',message.reception_date,' -\nsender id : ',message.sender_id,'\n',message.content)
-            print(f"{message.id} -\nReception date : {message.reception_date}\nsender id : {message.sender_id}\n{message.content}")
-    print('x. Main menu')
-    choice = input('Select an option: ')
-    if choice == 'x':
-        main_menu()
-    else:
-        print('Unknown option:', choice)
-
-def display_channels():
-    print('\nChannels list\n-------')
-    for channel in server.channels :
-        print(f"{channel.id} - {channel.name} : {channel.members_ids}")
-    print('\nn. Create channel\nx. Main menu')
-    choice = input('Select an option: ')
-    if choice == 'n':
-        create_channel()
-    elif choice == 'x':
-        main_menu()
-    else:
-        print('Unknown option:', choice)
-
-def main_menu():
-    print('\n1. See users\n2. See channels\n3. See messages\nx. Leave')
-    choice = input('Select an option: ')
-    if choice == 'x':
-        print('Bye!')
-    elif choice == '1':
-        display_users()
-    elif choice == '2':
-        display_channels()
-    elif choice == '3':
-        display_messages()
-    else:
-        print('Unknown option:', choice)
-
-
-
-
-
-print('=== Messenger ===')
-main_menu()
+client.main_menu()
 
 
 
