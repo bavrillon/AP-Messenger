f1ec0a7f4d35d378fc32485328f94ca2c5b1d952
Bastien Avrillon <bastien.avrillon@etu.minesparis.psl.eu>
2024-12-22 13:11:55 +0100

    Ajout de couleurs pour différencier les messages de l'IHM et les saisies utilisateurs

--- messenger.py
+++ messenger.py
@@ -100,20 +100,20 @@ class Server :
         self.save(JASON_FILE_NAME)
     
     def create_channel(self):
-        channel = input('Name of the new channel: ')
-        first_member_id = int(input('ID of the first user belonging to the new channel: '))
+        channel = input('\033[33mName of the new channel\033[0m: ')
+        first_member_id = int(input('\033[33mID of the first user belonging to the new channel: \033[0m'))
         member_ids = [first_member_id]
         while True:
-            choice = input('Add a member (yes/no) ? : ')
+            choice = input('\033[33mAdd a member (yes/no) ? : \033[0m')
             if choice == 'no':
                 break
             elif choice != 'yes':
-                print('Unknown option:', choice)
+                print('\033[33mUnknown option:\033[0m', choice)
                 continue
-            member_ids.append(int(input('ID of the next user belonging to the new channel: ')))
+            member_ids.append(int(input('\033[33mID of the next user belonging to the new channel: \033[0m')))
         n = max([channel.id for channel in server.channels])+1
         server.channels.append(Channel(n+1,channel,member_ids))
-        print('The new channel have successfully been created !')
+        print('\033[33mThe new channel have successfully been created !\033[0m')
         self.save(JASON_FILE_NAME)
 
     @classmethod
@@ -137,67 +137,67 @@ class Client :          #Messenger app
     
     def display_users(self):
         self.clear_screen()
-        print('\nUser list\n-------')
+        print('\033[33m\nUser list\n-------')
         for user in self.server.users :
             print(user.id,' - ',user.name)
         print('\nn. Create user\nb. Ban user\nx. Main menu')
-        choice = input('Select an option: ')
+        choice = input('Select an option: \033[0m')
         if choice == 'n':
-            new_names = input('Name of the new users (separators = ,) : ')
+            new_names = input('\033[33mName of the new users (separators = ,) : \033[0m')
             self.server.create_user(new_names)
             self.main_menu()
         elif choice == 'x':
             self.main_menu()
         elif choice == 'b':
-            banned_id = input('ID of the banned users (separators = ,) : ')
+            banned_id = input('\033[33mID of the banned users (separators = ,) : \033[0m')
             self.server.ban_user(banned_id)
             self.main_menu()
         else:
-            print('Unknown option:', choice)
+            print('\033[33mUnknown option:\033[0m', choice)
             time.sleep(0.8)
             self.display_users()
 
     def display_messages(self):
         self.clear_screen()
-        channel = int(input('Name of the channel: '))
-        print('\nMessages of the channel\n-------')
+        channel = int(input('\033[33mName of the channel: \033[0m'))
+        print('\033[31m\nMessages of the channel\n-------')
         for message in self.server.messages :
             if message.channel == channel :
         #       print(message.id',' -\nReception date : ',message.reception_date,' -\nsender id : ',message.sender_id,'\n',message.content)
                 print(f"{message.id} -\nReception date : {message.reception_date}\nsender id : {message.sender_id}\n{message.content}")
-        print('x. Main menu')
-        choice = input('Select an option: ')
+        print('\033[33mx. Main menu')
+        choice = input('Select an option: \033[0m')
         if choice == 'x':
-            self.display_messages()
+            self.main_menu()
         else:
-            print('Unknown option:', choice)
+            print('Unknown option:\033[0m', choice)
             time.sleep(0.8)
             self.main_menu()
 
     def display_channels(self):
         self.clear_screen()
-        print('\nChannels list\n-------')
+        print('\033[33m\nChannels list\n-------')
         for channel in self.server.channels :
             print(f"{channel.id} - {channel.name} : {channel.members_ids}")
         print('\nn. Create channel\nx. Main menu')
-        choice = input('Select an option: ')
+        choice = input('Select an option: \033[0m')
         if choice == 'n':
             self.server.create_channel()
             self.main_menu()
         elif choice == 'x':
             self.main_menu()
         else:
-            print('Unknown option:', choice)
+            print('\033[33mUnknown option:\033[0m', choice)
             time.sleep(0.8)
             self.display_channels()
     
     def main_menu(self):
         self.clear_screen()
-        print('=== Messenger ===')
+        print('\033[33m=== Messenger ===')
         print('\n1. See users\n2. See channels\n3. See messages\nx. Leave')
-        choice = input('Select an option: ')
+        choice = input('Select an option: \033[0m')
         if choice == 'x':
-            print('Bye!')
+            print('\033[33mBye!\033[0m')
         elif choice == '1':
             self.display_users()
         elif choice == '2':
@@ -205,7 +205,7 @@ class Client :          #Messenger app
         elif choice == '3':
             self.display_messages()
         else:
-            print('Unknown option:', choice)
+            print('\033[33mUnknown option:\033[0m', choice)
             time.sleep(0.8)
             self.main_menu()
     
