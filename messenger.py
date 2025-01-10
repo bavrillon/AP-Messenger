174f1822d215165e3485e369967a385d1c36b7cb
Bastien Avrillon <bastien.avrillon@etu.minesparis.psl.eu>
2025-01-10 22:39:32 +0100

    Assurer que Argparse reçoit 1 et 1 unique argument

--- messenger.py
+++ messenger.py
@@ -11,13 +11,15 @@ def initialisation_server() :           # Récupération du server (json) utilis
     parser.add_argument('--url',type=str, help = 'Enter the url of a remote server')
     args = parser.parse_args()
     # FORCER A AVOIR 1 ET 1 SEUL UNQUE ARGUMENT (NECESSAIRE) !!
-    if args.server != None :            # Local server
+    if (args.server == None) and  (args.url == None) :
+        return(None)
+    elif args.server != None :            # Local server
         print(f'Server json : {args.server}')
         sleep(0.5)
         jason_file_name = args.server
         local_server = LocalServer.load(jason_file_name)    
         return(local_server)
-    if args.url != None :            # Remote server
+    elif args.url != None :            # Remote server
         print(f'Remote Server : {args.url}')
         sleep(0.5)
         url = args.url
@@ -26,5 +28,8 @@ def initialisation_server() :           # Récupération du server (json) utilis
 
 if __name__ == "__main__":
     server = initialisation_server()
-    client = Client(server)
-    client.main_menu()  
+    if server == None :
+        print('Vous devez démarrer le programme avec un serveur. Lancer "--help" pour des informations supplémentaires')
+    else :
+        client = Client(server)
+        client.main_menu()  
